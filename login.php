a1d2809b4989ec8fbe9c976b0e9bdb88ff56ade3
MorganDavis0 <37461290+MorganDavis0@users.noreply.github.com>
2021-01-29 09:27:40 -0500

    Update login.php
    Updating because the html in this file was off for some reason.

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty(trim($_POST['username']))) {
         $username_err = 'Please enter username.';
     } 
-	else {
+    else {
         $username = trim($_POST["username"]);
     }
 
@@ -20,9 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty(trim($_POST['password']))) {
         $password_err = 'Please enter your password.';
     } 
-	else {
+    else {
         $password = trim($_POST['password']);
-		echo "inside empty password else.";
     }
 
     # Validate credentials
@@ -46,28 +45,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     # Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $name, $email, $hashed_password, $updated_at, $created_at);
                     if (mysqli_stmt_fetch($stmt)) {
-						#password is the ones specified, confirmed to already be hashed. Password verify will check the original against the hash.
+			#password is the ones specified, confirmed to already be hashed. Password verify will check the original against the hash.
                         if (password_verify($password, $hashed_password)) {
                             # Password is correct, so start a new session and save the variables.
                             session_start();
                             $_SESSION['username'] = $username;
                             $_SESSION['id'] = $id;
                             $_SESSION['password'] = $password;
-							#send user to website proper.
+			    #send user to website proper.
                             header("location: index.php");
                         } 
-						else {
+			else {
                             # Display an error message if password is not valid
                             $password_err = 'The password you entered was not valid.';
                         }
                     }
                 } 
-				else {
+		else {
                     # Display an error message if username doesn't exist
                     $username_err = 'No account found with that username.';
                 }
             } 
-			else {
+	    else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
@@ -81,35 +80,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" href="https:#maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <style type="text/css">
-            body{ font: 14px sans-serif;}
-            .wrapper{ margin: 0 auto; width: 350px; padding: 20px;}
-        </style>
-    </head>
-    <body>
-        <div class="wrapper">
-            <h2>Login</h2>
-            <p>Please fill in your credentials to login.</p>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                    <label>Username:<sup>*</sup></label>
-                    <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
-                    <span class="help-block"><?php echo $username_err; ?></span>
-                </div>
-                <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                    <label>Password:<sup>*</sup></label>
-                    <input type="password" name="password" class="form-control">
-                    <span class="help-block"><?php echo $password_err; ?></span>
-                </div>
-                <div class="form-group">
-                    <input type="submit" class="btn btn-primary" value="Submit">
-                </div>
-                <p>Don't have an account? Contact your local administrator.</p>
-            </form>
-        </div>
-    </body>
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Login</title>
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	    <style type="text/css">
+		body{ font: 14px sans-serif;}
+		.wrapper{ width: 350px; padding: 20px; margin-left: 40%;}
+	    </style>
+	</head>
+	<body>
+	    <div class="wrapper">
+		<h2>Login</h2>
+		<p>Please fill in your credentials to login.</p>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+		    <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+			<label>Username:<sup>*</sup></label>
+			<input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+			<span class="help-block"><?php echo $username_err; ?></span>
+		    </div>    
+		    <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+			<label>Password:<sup>*</sup></label>
+			<input type="password" name="password" class="form-control">
+			<span class="help-block"><?php echo $password_err; ?></span>
+		    </div>
+		    <div class="form-group">
+			<input type="submit" class="btn btn-primary" value="Submit">
+		    </div>
+		    <p>Don't have an account? Contact your administrator.</p>
+		</form>
+	    </div>    
+	</body>
 </html>
